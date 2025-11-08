bef6cf4edb92e46aeacfea00ad59718fbfd1906a
Chris24305 <christian.schaa@web.de>
2025-11-08 15:25:18 +0100

    h2finish

--- src/h2/H2_main.java
+++ src/h2/H2_main.java
@@ -2,6 +2,40 @@ package h2;
 
 public class H2_main {
 	public static void main(String[] args) {
-		
+		boolean x, y, a, b, c;
+		int input;
+		x = true;
+		y = true;
+		a = true;
+		b = true;
+		c = true;
+		input = 11;
+		if (input == 10 || input == 11) {
+			x = true;
+		} else {
+			x = false;
+		}
+		if (input == 1 || input == 11) {
+			y = true;
+		} else {
+			y = false;
+		}
+		if (x == true && y == true) {
+			a = true;
+		} else {
+			a = false;
+		}
+		if (x == true || y == true) {
+			b = true;
+		} else {
+			b = false;
+		}
+		c=!y;
+		/*if (y == false) {
+			c = true;
+		} else {
+			c = false;
+		} */
+		System.out.println("a: " + a + " b: " + b + " c: " + c);
 	}
 }
