d5992b3f502b1901316ff84c777cfae71fc84d5a
Chris24305 <christian.schaa@web.de>
2025-11-08 15:48:53 +0100

    finalh3

--- src/h3/H3_main.java
+++ src/h3/H3_main.java
@@ -1,5 +1,16 @@
 package h3;
 
 public class H3_main {
-
+	public static void main(String[] args) {
+		float celsiusFloat, fahrenheitFloat;
+		double celsiusDouble, fahrenheitDouble;
+		celsiusFloat = -14.2F;
+		fahrenheitFloat = 90.0F;
+		celsiusDouble = -14.2;
+		fahrenheitDouble = 90.0;
+		fahrenheitFloat = celsiusFloat *9/5 +32;
+		fahrenheitDouble = celsiusDouble *9/5 +32;
+		
+		// System.out.println("Float: "+fahrenheitFloat+", Double: "+fahrenheitDouble);
+	}
 }
